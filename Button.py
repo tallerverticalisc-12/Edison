f24e8ef810501869f9c6a22523017bd824f0892f
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 17:21:52 -0600

    Fixed

--- Button.py
+++ Button.py
@@ -2,7 +2,7 @@ import time
 import pyupm_grove as grove
 
 # Create the button object using GPIO pin 0
-button = grove.GroveButton(0)
+button = grove.GroveButton(3)
 
 # Read the input and print, waiting one second between readings
 while 1:
