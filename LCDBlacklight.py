0f791628f41fdfcdfe507c8959aa4f0157f3d1ce
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 17:34:12 -0600

    Fixed

--- LCDBlacklight.py
+++ LCDBlacklight.py
@@ -1,6 +1,6 @@
 import pyupm_i2clcd as lcd
 # Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)
-myLcd = lcd.Jhd1313m1(3, 0x3E, 0x62)
+myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 myLcd.setCursor(0,0)
 # RGB Blue
 #myLcd.setColor(53, 39, 249)
