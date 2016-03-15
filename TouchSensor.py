7cb0c2c9bcde70a57d40dd37b6d00510b8649222
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 17:30:46 -0600

    Fixed

--- TouchSensor.py
+++ TouchSensor.py
@@ -1,7 +1,7 @@
 import time
 import pyupm_ttp223 as ttp223
 # Create the TTP223 touch sensor object using GPIO pin 0
-touch = ttp223.TTP223(0)
+touch = ttp223.TTP223(3)
 # Check whether or not a finger is near the touch sensor and
 # print accordingly, waiting one second between readings
 while 1:
