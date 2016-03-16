23d1257dca6d6239d13315791744e86d590cf806
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 19:18:44 -0600

    Fix

--- Hello.py
+++ Hello.py
@@ -1,4 +1,6 @@
 import requests
+import json
 
-r = requests.get('https://federacionhemofilia.azurewebsites.net')
-print r.json() 
+r = requests.get('http://45.40.137.37:88/sensor')
+
+print (r.text)
