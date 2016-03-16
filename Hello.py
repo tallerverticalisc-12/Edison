496ab4ebad632b8551c7a28a7c2e330dabe91395
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 18:54:52 -0600

    Fixed

--- Hello.py
+++ Hello.py
@@ -1,6 +1,5 @@
 import requests
 import json
 
-r = requests.post('facebook.com', data = {'key':'value'})
-r = requests.get('facebook.com')
-r.json()
+r = requests.post('http://45.40.137.37:88/sensor', data = {'key':'value'})
+print (r.text)
