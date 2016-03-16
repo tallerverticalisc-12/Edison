644c112c41a50920d9a8556d089a4bfcd275322c
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 20:00:55 -0600

    Fix

--- ParkSystem.py
+++ ParkSystem.py
@@ -14,7 +14,6 @@ lcdDisplay.setCursor(0,0)
 dispPlaces = 20
 
 def updateInfo():
-    dispPlaces = dispPlaces
     s = requests.post('http://45.40.137.37:88/sensor', {"Places available":str(dispPlaces)})
     time.sleep(5)
     print (s.text)
