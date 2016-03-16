3e8f5d61d3f5ab11dcee1ed6667a9354ac90ace2
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 19:22:09 -0600

    Fix

--- Hello.py
+++ Hello.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 r = requests.get('http://45.40.137.37:88/test')
-s = requests.post('http://45.40.137.37:88/sensor', {id:"2"})
+s = requests.post('http://45.40.137.37:88/sensor', {"id":"2"})
 
 print (r.text)
 print (s.text)
