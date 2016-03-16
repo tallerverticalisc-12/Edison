084f6e75303e5329878c03d62286475c3c37a3b3
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 19:35:25 -0600

    Park System source code for the parking

---
+++ ParkSystem.py
@@ -0,0 +1,25 @@
+import requests
+import time
+import pyupm_grove as groove
+import pyupm_i2clcd as lcd
+import pyupm_ttp223 as ttp223
+
+touchSensor = ttp223.TTP223(2)
+
+button = grove.GroveButton(3)
+
+lcdDisplay = lcd.Jhd1313m1(0, 0x3E, 0x62)
+lcdDisplay.setCursor(0,0)
+
+
+int dispPlaces = 20
+
+while True:   
+    if touchSensor.isPresses()
+        dispPlaces -= 1
+    if button.value() == 1
+        dispPlaces += 1
+    lcdDisplay.write('Disponible parks: ' + dispPlaces)
+    
+    time.sleep(1)
+
