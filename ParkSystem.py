1d49a8a958621101a0e8f6a3150bb41f69acc1bb
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 19:37:31 -0600

    Fix

--- ParkSystem.py
+++ ParkSystem.py
@@ -15,9 +15,9 @@ lcdDisplay.setCursor(0,0)
 dispPlaces = 20
 
 while True:   
-    if touchSensor.isPressed()
+    if touchSensor.isPressed():
         dispPlaces -= 1
-    if button.value() == 1
+    if button.value() == 1:
         dispPlaces += 1
     lcdDisplay.write('Disponible parks: ' + dispPlaces)
     
