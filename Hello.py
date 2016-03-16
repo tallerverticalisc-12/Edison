8f33c3b61b8e8c0a83804ea0db4605f33c9f8e9f
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 18:36:32 -0600

    Fixed

--- Hello.py
+++ Hello.py
@@ -1,11 +1,14 @@
+from flask import Flask
 import json
 import requests
 
 @app.route('/sensor')
+
+def hello():
+    return "Hello World"
 
 if __name__ == '__main__':
 	app.run( 
 		host="http://45.40.137.37",
 		port=int("88")
 	)
-return "Hello World"
