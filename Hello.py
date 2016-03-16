07ff318436ba071e462b45ec2020f4364f6de8db
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-15 18:47:16 -0600

    Fixed

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 import requests
 
-r = request.post('http://45.40.137.37.88/sensor', data = {'key':'value'})
+r = requests.post('http://45.40.137.37.88/sensor', data = {'key':'value'})
 
-s = request.get('http://45.40.137.37.88/sensor')
+s = requests.get('http://45.40.137.37.88/sensor')
 
 print s;
