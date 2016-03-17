583d6304ff2e5240f4c4ea511818aabe25c9bf02
gaboTruGomez <gabriel.trugomez@outlook.com>
2016-03-17 15:11:48 -0600

    tests

--- grove_servo.py
+++ grove_servo.py
@@ -3,13 +3,14 @@ import pyupm_servo as servo
  
 # Create the servo object using D5
 gServo = servo.ES08A(5)
+gServo.haltPwm()
 for i in range(0,5):
     # Set the servo arm to 0 degrees
     gServo.setAngle(0)
     print 'Set angle to 0'
     time.sleep(1)
     # Set the servo arm to 90 degrees
-    gServo.setAngle(90)
+    gServo.setAngle(200)
     print 'Set angle to 90'
     time.sleep(1)
 # Delete the servo object
