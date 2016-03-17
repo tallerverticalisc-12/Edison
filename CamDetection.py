72c7f088d6c11defe7f82a5ba7e34162bf4e2724
gaboTruGomez <gabriel.trugomez@outlook.com>
2016-03-17 16:08:33 -0600

    testing

--- CamDetection.py
+++ CamDetection.py
@@ -3,18 +3,8 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+ret, img = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("image_testv2", img)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
 cap.release()
-cv2.destroyAllWindows()
