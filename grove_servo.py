a0ae97887ceefa7ea900930c9c3ae11347dfdfb2
gaboTruGomez <gabriel.trugomez@outlook.com>
2016-03-17 15:12:27 -0600

    fix

--- grove_servo.py
+++ grove_servo.py
@@ -10,7 +10,7 @@ for i in range(0,5):
     print 'Set angle to 0'
     time.sleep(1)
     # Set the servo arm to 90 degrees
-    gServo.setAngle(200)
+    gServo.setAngle(90)
     print 'Set angle to 90'
     time.sleep(1)
 # Delete the servo object
