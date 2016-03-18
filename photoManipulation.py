2cc008b29e26e6e3a736012a5f52fdd08d6e7744
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-17 20:56:15 -0600

    Fix

--- photoManipulation.py
+++ photoManipulation.py
@@ -1,4 +1,4 @@
-import pytesseract
+from pytesser import *
 from PIL import Image
 from numpy import *
 import cv2
@@ -9,8 +9,6 @@ original = cv2.medianBlur(original, 5)
 #img2Gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 
 final = cv2.adaptiveThreshold(original, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-cv2.imwrite('test_imageV2_gray.jpg', final)
-
 #img = Image.open('test_imageV2_gray.jpg')
 #img.save('test_imageV2.tiff')
 #im = Image.open(img)
@@ -19,10 +17,10 @@ cv2.imwrite('test_imageV2_gray.jpg', final)
 #text = image_file_to_string(image_file, graceful_errors=True)
 #print "=====output=======\n", text
 
-img = Image.open('test_imageV2_gray.jpg')
-cv2.imwrite('final.tiff', img)
-im = Image.open('final.tiff')
-text = pytesseract.image_to_string(im)
-text = pytesseract.image_file_to_string(image_file)
-text = pytesseract.image_file_to_string(image_file, graceful_errors=True)
+cv2.imwrite('test_imageV2.tiff', final)
+image_file = 'test_imageV2.tiff'
+im = Image.open(image_file)
+text = image_to_string(im)
+text = image_file_to_string(image_file)
+text = image_file_to_string(image_file, graceful_errors=True)
 print "=====output=======\n", text
