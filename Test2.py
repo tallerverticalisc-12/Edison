43012265037410e15264d6ca7f2f36e4747d44db
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 10:21:23 -0600

    Fix

--- Test2.py
+++ Test2.py
@@ -1,8 +1,26 @@
-import cv2
 from PIL import Image
-import numpy as np
+from numpy import *
+import cv2
 from pytesseract import *
 
-imgToAnalyze = cv2.imread('imgToAnalyze.jpg')
-imgToAnalyze = cv2.medianBlur(imgToAnalyze, 5)
-thresh1 = cv2.threshold(imgToAnalyze,127,255,cv2.THRESH_BINARY)
+imgToAnalyze = cv2.imread('imgToAnalyze.jpg', 0)
+imgToAnalyze = cv2.medianBlur(original, 5)
+
+#img2Gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+
+imgThreshold = cv2.adaptiveThreshold(original, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+#img = Image.open('test_imageV2_gray.jpg')
+#img.save('test_imageV2.tiff')
+#im = Image.open(img)
+#text = image_to_string(im)
+#text = image_file_to_string(image_file)
+#text = image_file_to_string(image_file, graceful_errors=True)
+#print "=====output=======\n", text
+
+cv2.imwrite('imgConverted.tif', imgThreshold)
+imgFile = 'imgConverted.tif'
+imgFinal = Image.open(imgFile).load()
+text = image_to_string(im)
+text = image_file_to_string(imgFile)
+text = image_file_to_string(imgFile, graceful_errors=True)
+print "=====output=======\n", text
