790b382c4a8170f1dfd58d56b23c488afb5850c6
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 10:48:59 -0600

    Fix

--- photoManipulation.py
+++ photoManipulation.py
@@ -6,21 +6,24 @@ from PIL import Image
 from PIL import ImageFilter
 from StringIO import StringIO
 
-cap = cv2.VideoCapture(0)
-tempFrames = 15
 
-for i in range(0, tempFrames):
-	ret, img = cap.read()	
+def captureImage():
+    cap = cv2.VideoCapture(0)
+    tempFrames = 15
+    
+    for i in range(0, tempFrames):
+    	ret, img = cap.read()	
 
-if ret:
-	cv2.imwrite("imgToAnalyze.jpg", img)
+    if ret:
+	    cv2.imwrite("imgToAnalyze.jpg", img)
 
-cap.release()
-
+    cap.release()
+    
 def convertImage():
     imgToAnalyze = Image.open('imgToAnalyze.jpg')
     imgToAnalyze.filter(ImageFilter.SHARPEN)
     
     print pytesseract.image_to_string(imgToAnalyze)
     
+captureImage()
 convertImage()
