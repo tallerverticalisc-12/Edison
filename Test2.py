a72e40ffa636fcbbdae4088be607496f81de8421
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 09:14:33 -0600

    Fix

--- Test2.py
+++ Test2.py
@@ -4,9 +4,6 @@ from PIL import Image
 from PIL import ImageFilter
 from StringIO import StringIO
 
-image = _get_image('phototest.tif')
+image = Image.open('phototest.tif')
 image.filter(ImageFilter.SHARPEN)
 print pytesseract.image_to_string(image)
-
-def _get_image(url):
-    return Image.open(StringIO(requests.get(url).content))
