7da57ac6c52f36d47e02da4c92c6a35227eb3ee2
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 12:04:55 -0600

    Fixed bugs

--- ParkSystem.py
+++ ParkSystem.py
@@ -23,7 +23,7 @@ dispPlaces = 20
 def requestPlace():
     r = requests.get('http://45.40.137.37:88/bestZone')
     print (r.text)
-    parsedJSON = r.loads(r)
+    parsedJSON = json.loads(r)
     print(parsedJSON['zone'])
 
 #Request to the server that has the information about the parking zones
