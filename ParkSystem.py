fa3af0d9792b5aa4c467748f4bacfbac8255faa7
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 12:50:08 -0600

    Code optimization

--- ParkSystem.py
+++ ParkSystem.py
@@ -3,23 +3,23 @@ import time
 import json
 import pyupm_grove as grove
 import pyupm_i2clcd as lcd
-import pyupm_servo as servo
 import pyupm_ttp223 as ttp223
 
-#light_sensor = grove.GroveLight(0)
+#Grove Touch Sensor variable
 touchSensor = ttp223.TTP223(2)
-button = grove.GroveButton(3)
-gServo = servo.ES08A(5)
 
+#Grove Button variable
+button = grove.GroveButton(3)
+
+#Grove LCD Blacklight display variable
 lcdDisplay = lcd.Jhd1313m1(0, 0x3E, 0x62)
 lcdDisplay.setCursor(0,0)
 
-touchCounter = 0
-buttonCounter = 0
-
-dispPlaces = 20
+#Variable to store the json from the server
 r = requests.get('http://45.40.137.37:88/bestZone')
 parsedJSON = json.loads(r.text)
+
+#Variable to store a formatted json to print
 global zone
 zone =  parsedJSON['zone']
 
@@ -38,26 +38,21 @@ def updateInfo(info):
 #Updates if a car leaves or enters to a parking zone    
 def parkInfo():
     global dispPlaces
-    global touchCounter
-    global buttonCounter
     
+    #Detects when a car is in the a determinate spot
     if touchSensor.isPressed():
-        if dispPlaces > 0:
-            dispPlaces += 1
-            updateInfo(1)
-                
+        updateInfo(1)
+    
+    #Detects when a car enters to the parking lot             
     if button.value() == 1:
-        if dispPlaces > 0:
-            dispPlaces -= 1
-            lcdDisplay.write('Welcome!!!')
-            time.sleep(2)
-            lcdDisplay.clear()
-            lcdDisplay.write('Go to: ' + str(zone))
-            updateInfo(0)
-            time.sleep(5)
-            requestPlace()
-            
-    lcdDisplay.clear()
+        lcdDisplay.write('Welcome!!!')
+        time.sleep(2)
+        lcdDisplay.clear()
+        lcdDisplay.write('Go to: ' + str(zone))
+        updateInfo(0)
+        time.sleep(5)
+        requestPlace()    
+        lcdDisplay.clear()
 
 while True:
     parkInfo()
