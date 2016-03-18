cfc9eb1c33c40ffe3617192fe91cfb6c74b5ee7c
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 09:46:31 -0600

    Fix

--- Test2.py
+++ Test2.py
@@ -10,4 +10,4 @@ originalPic = cv.LoadImageM("picToAnalyze.jpg")
 resize = cv.CreateMat(originalPic.rows/ 10, originalPic.cols / 10, originalPic.type) 
 
 cv.Resize(originalPic, resize)
-th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(originalPic,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
