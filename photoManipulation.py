2b8a58c4fa348ffcb074aae7e86f0a948a044c2a
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-17 19:55:26 -0600

    Fix

--- photoManipulation.py
+++ photoManipulation.py
@@ -7,13 +7,20 @@ original = cv2.imread('test_imageV2.jpg')
 img2Gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 
 cv2.imwrite('test_imageV2_gray.jpg', original)
-cv2.Threshold(img2Gray, binary_image,128,255, CV_THRESH_OTSU)
+final = cv2.threshold(img2Gray, ,128,255, cv2.THRESH_BINARY)
 
 img = Image.open('test_imageV2_gray.jpg')
 img.save('test_imageV2.tiff')
-im = Image.open(img)
-text = image_to_string(im)
-text = image_file_to_string(image_file)
-text = image_file_to_string(image_file, graceful_errors=True)
-print "=====output=======\n", text
+#im = Image.open(img)
+#text = image_to_string(im)
+#text = image_file_to_string(image_file)
+#text = image_file_to_string(image_file, graceful_errors=True)
+#print "=====output=======\n", text
 
+img = final
+img.save('test_imageV2.tiff')
+#im = Image.open(img)
+#text = image_to_string(im)
+#text = image_file_to_string(image_file)
+#text = image_file_to_string(image_file, graceful_errors=True)
+#print "=====output=======\n", text
