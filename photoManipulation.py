5405fc47d9ef5da643722acafceab70035f3e826
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 10:45:16 -0600

    Fix

--- photoManipulation.py
+++ photoManipulation.py
@@ -8,18 +8,14 @@ from StringIO import StringIO
 
 cap = cv2.VideoCapture(0)
 tempFrames = 15
-
 for i in range(0, tempFrames):
 	ret, img = cap.read()	
-
 if ret:
 	cv2.imwrite("imgToAnalyze.jpg", img)
-
 cap.release()
 
 def convertImage():
     imgToAnalyze = Image.open('imgToAnalyze.jpg')
-    imgToAnalyze = cv2.medianBlur(imgToAnalyze, 5)
     imgToAnalyze.filter(ImageFilter.SHARPEN)
     
     print pytesseract.image_to_string(imgToAnalyze)
