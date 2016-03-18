acf8d6a6a17266b919537d2d62d51384cfbb35c2
marysolsnz2 <marysol_12_12@hotmail.com>
2016-03-18 12:46:00 -0600

    Update LCDBlacklight.py

--- LCDBlacklight.py
+++ LCDBlacklight.py
@@ -1,3 +1,19 @@
+#LCDBlacklight.py
+#Copyright (C) 2016  Smart Parking
+
+#TThis program is free software: you can redistribute it and/or modify
+    #it under the terms of the GNU General Public License as published by
+    #the Free Software Foundation, either version 3 of the License, or
+    #(at your option) any later version.
+
+    ##This program is distributed in the hope that it will be useful,
+    #but WITHOUT ANY WARRANTY; without even the implied warranty of
+    #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    #GNU General Public License for more details.
+
+    #You should have received a copy of the GNU General Public License
+    #along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import pyupm_i2clcd as lcd
 import time
 # Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)
