8870b8fbf861bcbfec274aeadaeb3dd295c45e1e
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-17 19:28:24 -0600

    Fix

--- photoManipulation.py
+++ photoManipulation.py
@@ -1,9 +1,9 @@
 #Fix
-import cv2 as cv2
+import cv2
 from PIL import Image
 from pytesser import *
 
-original = cv2.imread("test_imageV2.png")
+original = cv2.imread("test_imageV2.jpg")
 
 resize = cv2.cv.CreateMat(original.rows/ 10, original.cols / 10, original.type) 
 
@@ -13,7 +13,7 @@ img2Gray = cv2.CvtColor(original,gray,CV_RGB2GRAY)
 
 cv2.cv.Threshold(img2Gray, binary_image,128,255, CV_THRESH_OTSU)
 
-img = Image.open('test_imageV2.png')
+img = Image.open('test_imageV2.jpg')
 img.save('test_imageV2.tiff')
 im = Image.open(img)
 text = image_to_string(im)
