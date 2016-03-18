0e0a57e94f8a2bbd438709179af57e0f977c19fa
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-17 19:45:09 -0600

    Fix

--- photoManipulation.py
+++ photoManipulation.py
@@ -1,17 +1,21 @@
-import cv
 from PIL import Image
-from pytesser import *
+from numpy import *
+from pylab import *
+import cv2
 
-original = cv.LoadImage("test_imageV2.jpg")
+original = cv2.imread('test_imageV2.jpg')
 
-img2Gray = CvtColor(original,gray,CV_RGB2GRAY)
+gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 
-cv.Threshold(img2Gray, binary_image,128,255, CV_THRESH_OTSU)
+cv2.imwrite('test_imageV2_gray.jpg')
 
-img = Image.open('test_imageV2.jpg')
+#cv.Threshold(img2Gray, binary_image,128,255, CV_THRESH_OTSU)
+
+img = Image.open('test_imageV2_gray.jpg')
 img.save('test_imageV2.tiff')
 im = Image.open(img)
 text = image_to_string(im)
 text = image_file_to_string(image_file)
 text = image_file_to_string(image_file, graceful_errors=True)
 print "=====output=======\n", text
+
