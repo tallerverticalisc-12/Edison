8c9984e7ec4985032e41a8742c24eb5202173a00
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 10:09:39 -0600

    Fix

--- Test2.py
+++ Test2.py
@@ -3,8 +3,4 @@ from PIL import Image
 from pytesseract import *
 
 imgToAnalyze = cv2.imread('imgToAnalyze.jpg')
-
-imgGrayScale = cv2.cvtColor(imgToAnalyze, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('imgGrayScale.jpg', imgGrayScale)
-
-#imgThreshold = cv2.adaptiveThreshold(imgGrayScale,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+imgGrayScale = cv2.cvtColor(imgToAnalyze, gray, CV_RGB2GRAY)
