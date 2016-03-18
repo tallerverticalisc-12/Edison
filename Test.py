662efeb1584b5418901676b55a07b97de3eca48c
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-17 23:44:51 -0600

    Fix

--- Test.py
+++ Test.py
@@ -1,5 +1,7 @@
 import pytesseract
 from PIL import Image 
-print pytesseract.image_to_string(Image.open('Letters.jpg'))
-load(Image.open('Letters.jpg'))
-#fs
+
+imageTest = Image.open('Letters.jpg')
+imageTest.load()
+
+print(pytesseract.image_to_string(imageTest))
