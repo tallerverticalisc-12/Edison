3a35664a5bc4d7563613e718f4a136395ea6a6fd
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 00:38:20 -0600

    Fix

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
 import Image
-from tesseract import image_to_string
+from tesseract import *
 print image_to_string(Image.open('test.png'))
 print image_to_string(Image.open('test-european.jpg'), lang='fra')
