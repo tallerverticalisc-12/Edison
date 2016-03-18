7485a6a40905bdb58b282f22411aaa44e96b4d9a
gaboTruGomez <gabriel.trugomez@outlook.com>
2016-03-18 02:46:36 -0600

    servo test

--- grove_sensorV2.py
+++ grove_sensorV2.py
@@ -1,16 +1,25 @@
 import time
 import pyupm_servo as servo
- 
+ import pyupm_grove as grove
+
 # Create the servo object using D5
 gServo = servo.ES08A(5)
-for i in range(0,5):
+light_sensor = grove.GroveLight(0)
+
+while(true):
+	if(light_sensor.value() <= 2):
+		gServo.setAngle(90)
+	else:
+		gServo.setAngle(0)
+
+#for i in range(0,5):
     # Set the servo arm to 0 degrees
-    gServo.setAngle(0)
-    print 'Set angle to 0'
-    time.sleep(1)
+#    gServo.setAngle(0)
+#    print 'Set angle to 0'
+#    time.sleep(1)
     # Set the servo arm to 90 degrees
-    gServo.setAngle(90)
-    print 'Set angle to 90'
-    time.sleep(1)
+#    gServo.setAngle(90)
+#    print 'Set angle to 90'
+#    time.sleep(1)
 # Delete the servo object
 del gServo
