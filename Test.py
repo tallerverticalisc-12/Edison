9cbdd04b63e2d5699326fa9a273902a70e3f03ff
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-17 23:59:32 -0600

    Fix

--- Test.py
+++ Test.py
@@ -2,5 +2,7 @@ import pytesseract as pt
 import Image as im
 from PIL import Image as PILim
 
-print pt.image_to_string(im.open("Letters.jpg"))
+
+
+print pt.image_to_string(im.open("Letters.jpg").split())
 print pt.image_to_string(PILim.open("Letters.jpg"))
