80d5e1f06b6ce01f68935e94e94f9581dda8f3d4
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 09:43:52 -0600

    Fix

--- Test2.py
+++ Test2.py
@@ -9,5 +9,4 @@ originalPic = cv.LoadImageM("picToAnalyze.jpg")
 resize = cv.CreateMat(originalPic.rows/ 10, originalPic.cols / 10, originalPic.type) 
 
 cv.Resize(originalPic, resize)
-CvtColor(originalPic,gray,CV_RGB2GRAY)
 cvThreshold(image, binary_image,128,255,CV_THRESH_OTSU)
