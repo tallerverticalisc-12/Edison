80ca753922ba230485014d52fb724980d70102e6
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-17 21:09:31 -0600

    Fix

--- photoManipulation.py
+++ photoManipulation.py
@@ -17,8 +17,8 @@ final = cv2.adaptiveThreshold(original, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2
 #text = image_file_to_string(image_file, graceful_errors=True)
 #print "=====output=======\n", text
 
-cv2.imwrite('test_imageV2.tiff', final)
-image_file = 'test_imageV2.tiff'
+cv2.imwrite('test_imageV2.tif', final)
+image_file = 'test_imageV2.tif'
 im = Image.open(image_file)
 text = image_to_string(im)
 text = image_file_to_string(image_file)
