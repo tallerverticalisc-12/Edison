3c5e7af71ba8d2108178605f3028f7e11b07337a
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-17 19:05:19 -0600

    Fix

--- PhotoCapture.py
+++ PhotoCapture.py
@@ -1,6 +1,15 @@
-from cv2 import *
-# initialize the camera
-cam = VideoCapture(0)   # 0 -> index of camera
-s, img = cam.read()
-if s:    # frame captured without any errors
-    imwrite("filename.jpg",img) #save image
+import cv2.cv as cv
+import time
+
+cv.NamedWindow("camera", 1)
+
+capture = cv.CaptureFromCAM(0)
+
+i = 0
+while True:
+    img = cv.QueryFrame(capture)
+    cv.ShowImage("camera", img)
+    cv.SaveImage('pic{:>05}.jpg'.format(i), img)
+    if cv.WaitKey(10) == 27:
+        break
+    i += 1
