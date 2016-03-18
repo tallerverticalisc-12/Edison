5081215002e7594c69a851ec1b11202ad6e75442
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 06:48:28 -0600

    Fix

---
+++ Test2.py
@@ -0,0 +1,11 @@
+import Image
+from pytesseract import image_to_string
+
+photo = Image.open('phototest.tif')
+photo.load()
+photo.split()
+
+print image_to_string(Image.open(image)
+#Save
+#Save 
+#Save
