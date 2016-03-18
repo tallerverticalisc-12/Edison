69cbd4603d58453b9447b16a313bb827188f7676
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 09:31:34 -0600

    Fix

--- photoManipulation.py
+++ photoManipulation.py
@@ -1,26 +1,12 @@
+import pytesseract
+import requests
 from PIL import Image
-from numpy import *
-import cv2
-from pytesseract import *
+from PIL import ImageFilter
+from StringIO import StringIO
 
-original = cv2.imread('test_imageV2.jpg', 0)
-original = cv2.medianBlur(original, 5)
-
-#img2Gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-
-final = cv2.adaptiveThreshold(original, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-#img = Image.open('test_imageV2_gray.jpg')
-#img.save('test_imageV2.tiff')
-#im = Image.open(img)
-#text = image_to_string(im)
-#text = image_file_to_string(image_file)
-#text = image_file_to_string(image_file, graceful_errors=True)
-#print "=====output=======\n", text
-
-cv2.imwrite('test_imageV2.tif', final)
-image_file = 'test_imageV2.tif'
-im = Image.open(image_file).load()
-text = image_to_string(im)
-#text = image_file_to_string(image_file)
-#text = image_file_to_string(image_file, graceful_errors=True)
-#print "=====output=======\n", text
+def getImage():
+    image = Image.open('test_photoV2.jpg')
+    image.filter(ImageFilter.SHARPEN)
+    print pytesseract.image_to_string(image)
+    
+getImage()
