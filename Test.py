b621a5e31b3ca92a191fb0723d3437f72424547c
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 05:22:31 -0600

    Fix

--- Test.py
+++ Test.py
@@ -1,9 +1,7 @@
-import Image
+import PIL import Image
 from pytesseract import image_to_string
 
 image = Image.open('phototest.tif')
-image.load()
-image.split()
 
 print image_to_string(Image.open(image)
 #Save
