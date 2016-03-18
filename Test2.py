7303e87cbfcb63ebaacd7595a60e4bfe10349e66
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 10:33:55 -0600

    Fix

--- Test2.py
+++ Test2.py
@@ -19,7 +19,7 @@ imgThreshold = cv2.adaptiveThreshold(imgToAnalyze, 255, cv2.ADAPTIVE_THRESH_GAUS
 
 cv2.imwrite('imgConverted.tif', imgThreshold)
 imgFile = 'imgConverted.tif'
-imgFinal = Image.open(imgFile)
+imgFinal = Image.open(imgFile).load()
 text = image_to_string(imgFinal)
 #text = image_file_to_string(imgFile)
 #text = image_file_to_string(imgFile, graceful_errors=True)
