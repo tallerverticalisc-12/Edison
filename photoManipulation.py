03171efcd3c97eb7714923e97c164d7ced31cb50
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 10:27:51 -0600

    Fix

--- photoManipulation.py
+++ photoManipulation.py
@@ -5,8 +5,9 @@ from PIL import ImageFilter
 from StringIO import StringIO
 
 def getImage():
-    image = Image.open('test_imageV2.jpg')
+    image = Image.open('imgToAnalyze.jpg')
     image.filter(ImageFilter.SHARPEN)
+    
     print pytesseract.image_to_string(image)
     
 getImage()
