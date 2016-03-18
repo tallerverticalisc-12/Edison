709913c15fa5708a0184a730cb402a8e626789d4
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-17 23:57:10 -0600

    Fix

--- Test.py
+++ Test.py
@@ -1,7 +1,6 @@
-from PIL import Image
-from pytesser import *
+import pytesseract as pt
+import Image as im
+from PIL import Image as PILim
 
-imageTest = Image.open('Letters.jpg')
-imageTest.load()
-
-print(pytesseract.image_to_string(imageTest))
+print pt.image_to_string(im.open("test_img.jpg"))
+print pt.image_to_string(PILim.open("test_img.jpg"))
