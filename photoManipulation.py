f38e43aaeecbf191a9dce91fdd036cde828ec54a
Samuel Osuna <samosunaz@MacBook-Pro-de-Samuel.local>
2016-03-18 10:42:36 -0600

    Merge Code

--- photoManipulation.py
+++ photoManipulation.py
@@ -3,11 +3,24 @@ import requests
 from PIL import Image
 from PIL import ImageFilter
 from StringIO import StringIO
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+tempFrames = 15
+
+for i in range(0, tempFrames):
+	ret, img = cap.read()	
+
+if ret:
+	cv2.imwrite("imgToAnalyze.jpg", img)
+
+cap.release()
 
 def getImage():
-    image = Image.open('picToAnalyze.jpg')
-    image.filter(ImageFilter.SHARPEN)
+    imgToAnalyze = Image.open('imgToAnalyze.jpg')
+    imgToAnalyze.filter(ImageFilter.SHARPEN)
     
-    print pytesseract.image_to_string(image)
+    print pytesseract.image_to_string(imgToAnalyze)
     
 getImage()
